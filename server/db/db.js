e86e233feb7e7b9e3fb01ad6bd6301b17eeb8281
jlhline <jlhline@gmail.com>
2022-02-02 15:27:57 -0500

    addded other uri to db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require("sequelize");
 //Change made: adding my username to connection uri(no password)
-const db = new Sequelize("postgres://JohnHaberstroh@localhost:5432/messenger", {
-  logging: false,
-});
+const db = new Sequelize(
+  "postgres://JohnHaberstroh@localhost:5432/messenger" ||
+    "postgres://localhost:5432/messenger",
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
