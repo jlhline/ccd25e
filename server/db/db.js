4796c522d15d9e231e9bd9142248fa13fe04d7dc
jlhline <jlhline@gmail.com>
2022-02-02 15:36:10 -0500

    changed db env arguments after observing past tests

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 //Change made: adding my username to connection uri(no password)
 const db = new Sequelize(
-  "postgres://JohnHaberstroh@localhost:5432/messenger" ||
-    "postgres://localhost:5432/messenger",
+  process.env.DATABASE_URL ||
+    "JohnHaberstroh@postgres://localhost:5432/messenger",
   {
     logging: false,
   }
