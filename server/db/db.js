02294ff0bb2808cfc878772947f1019f65d6340c
jlhline <jlhline@gmail.com>
2022-02-04 14:45:14 -0500

    db uri fix

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 //Change made: adding my username to connection uri(no password)
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-    "postgres://JohnHaberstroh@localhost:5432/messenger",
+    `postgres://${process.env.DATABASE_USER}@localhost:5432/messenger`,
   {
     logging: false,
   }
